314ed2298ce56fd3183c5709e858ae8bd3934615
Kacper Skirel <pskirel@wp.pl>
2023-01-16 17:01:55 +0000

    ok this didn't work

---
+++ docs/_app/immutable/chunks/1-40354a4d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-291ca760.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-a61668f8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b97b346b.js";export{t as component};
